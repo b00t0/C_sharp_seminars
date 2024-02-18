5d35fde46177f2996f06279d3d24ed00809658e2
Igor Bobkov <bobkovid@gmail.com>
2024-02-18 17:52:27 +0300

    Program.cs committed

--- Task01_Sem1/Program.cs
+++ Task01_Sem1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход
+//принимает два целых числа и проверяет,
+//является ли первое число квадратом второго.
+//a = 25, b = 5 => да
+//a = 2, b = 10 => нет
+//a = 9, b = -3 => да
+//a = -3, b = 9 => нет
+
+Console.WriteLine("Enter the integer number a");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the integer number b");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+string result = (num1 == num2 * num2) ? "да" : "нет";
+
+Console.WriteLine($"a = {num1}, b = {num2} => {result}");
