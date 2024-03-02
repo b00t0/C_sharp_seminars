1ebd6cd23a8375ccbde1549818bfd41c21787e63
Igor Bobkov <bobkovid@gmail.com>
2024-03-02 17:52:48 +0300

    Task03_Sem5

---
+++ Task03_Sem5/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задайте двумерный массив из целых чисел. Сформируйте новый одномерный массив, 
+// состоящий из средних арифметических значений по строкам двумерного массива.
+
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i], 10}");
+    }
+}
+
+double[] AverageRows(int[,] matrix)
+{
+    double[] array = new double[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        double average = sum / matrix.GetLength(1);
+        array[i] = average;
+    }
+    return array;
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 0, 10);
+PrintMatrix(array2d);
+
+Console.WriteLine();
+
+double[] newArray = AverageRows(array2d);
+
+PrintArray(newArray);
