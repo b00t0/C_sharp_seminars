7413e2439d34e040c492c95b22a19512e2cc7a52
Igor Bobkov <bobkovid@gmail.com>
2024-03-04 23:45:26 +0300

    Task01_Sem6

---
+++ Task01_Sem6/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте массив символов (тип char []). Создайте строку из 
+// символов этого массива.
+// Конструктор строки вида string(char []) не использовать.
+
+
+
+string GetStringFromCharArray(char[] chars)
+{
+    string str = string.Empty;
+
+    for (int i = 0; i < chars.Length; i++)
+    {
+        str += chars[i];
+    }
+    return str;
+}
+
+char[] charsArray = { 'a', 'b', 'c', 'd' };
+
+Console.WriteLine(GetStringFromCharArray(charsArray));
+
