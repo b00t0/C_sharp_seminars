ecda1bd4ee7829916645b173ad934d98adff823e
Igor Bobkov <bobkovid@gmail.com>
2024-03-09 19:01:48 +0300

    Task03_Sem7

---
+++ Task03_Sem7/Program.cs
@@ -0,0 +1,22 @@
+﻿// Считать строку с консоли, содержащую латинские буквы. 
+// Вывести на экран согласные буквы этой строки.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+
+
+void ShowConsonants(string str)
+{
+    string vowels = "aeiouy";
+    if (str.Length == 0) return;
+    if (!vowels.Contains(str[0]))
+    {
+        Console.Write($"{str[0]} ");
+    }
+    ShowConsonants(str.Substring(1));
+}
+
+
+Console.WriteLine("Enter the text: ");
+string str = Console.ReadLine();
+
+ShowConsonants(str);
