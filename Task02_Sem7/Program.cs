f0f86eca685be5b24585030aa267d9b66c0b7020
Igor Bobkov <bobkovid@gmail.com>
2024-03-09 19:01:33 +0300

    Task02_Sem7

---
+++ Task02_Sem7/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая будет принимать на вход число и 
+// возвращать сумму его цифр.
+// Указание
+// Использовать рекурсию.
+
+int SumNumbers(int number)
+{
+	if(number == 0) return 0;
+	return number % 10 + SumNumbers(number / 10);
+}
+
+Console.WriteLine(SumNumbers(123));
+
