aec4093b467aef1f13be63755ef1019c4bff2dfc
Igor Bobkov <bobkovid@gmail.com>
2024-03-09 19:01:16 +0300

    Task01_Sem7

---
+++ Task01_Sem7/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от 1 до N.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    NaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
+
+
+Console.WriteLine("Enter the natural number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(number);
+
